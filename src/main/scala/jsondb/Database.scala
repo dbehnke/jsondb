6d52aca8c50fa021fc397fbe70f17fa549d552f1
Dave Behnke <dbehnke74@gmail.com>
2014-01-07 15:33:23 -0500

    add support for more JSON types

--- src/main/scala/jsondb/Database.scala
+++ src/main/scala/jsondb/Database.scala
@@ -21,6 +21,14 @@ object Database {
     ConnectionPool.add('tomcat,"jdbc:h2:mem:hello", "user", "pass", settings)
   }
 
+/*
+case class JString(s: String) extends JValue
+case class JDouble(num: Double) extends JValue
+case class JDecimal(num: BigDecimal) extends JValue
+case class JInt(num: BigInt) extends JValue
+case class JBool(value: Boolean) extends JValue
+*/
+
   def queryJSON(dbpool : Any, s : SQLSyntax,
    typedefs : Seq[String]) : String = {
     NamedDB(dbpool) readOnly { implicit session => {
@@ -42,9 +50,33 @@ object Database {
                   case _ => JNull
                 }
               }
+              case "datetime" => {
+                rs.dateTimeOpt(i) match {
+                  case Some(t) => JString(t.toString)
+                  case _ => JNull
+                }
+              }
               case "int" => {
-                rs.intOpt(i) match {
+                rs.longOpt(i) match {
                   case Some(t) => JInt(t)
+                  case _ => JNull
+                }
+              }
+              case "double" => {
+                rs.doubleOpt(i) match {
+                  case Some(t) => JDouble(t)
+                  case _ => JNull
+                }
+              }
+              case "decimal" => {
+                rs.bigDecimalOpt(i) match {
+                  case Some(t) => JDecimal(t)
+                  case _ => JNull
+                }
+              }
+              case "boolean" => {
+                rs.booleanOpt(i) match {
+                  case Some(t) => JBool(t)
                   case _ => JNull
                 }
               }
